5738b1f59ac500896b51a9fcbb920db6fb78b35c
Parikshit Sharma <sharmaparikshit405@gmail.com>
2025-08-27 22:37:20 +0530

    c file

---
+++ hi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+     // datatype in float
+    // float b = 10;
+    // printf("Hello, World!\n");
+    // // Adding two numbers
+    // printf("Sum: %.2f\n", a + b);
+    // return 0;
+// print the length of the function inside the bracket that is 6 include \ also
+    printf("hello");
+
+
+}
