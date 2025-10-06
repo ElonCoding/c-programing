2f690bd321bb5a14c0cae230cf8af53160250118
Parikshit Sharma <sharmaparikshit405@gmail.com>
2025-10-06 13:40:59 +0530

    lect19

---
+++ lect20/char.c
@@ -0,0 +1,46 @@
+#include <string.h>
+    int main()
+    {
+        char name[] = "madam";
+        int i = 0;
+        int ln = strlen(name);
+        printf("%d", ln);
+    }
+
+    for (int i = 0; name[i] != '\0'; i++)
+    {
+        printf("%c", name[i]);
+    }
+
+    char data[10];
+    for (int i = 0; name[i] != '\0'; i++)
+    {
+        data[i] = name[i];
+    }
+    printf("%s", data);
+
+    int ln = strlen(name);
+    int isTrue = 1;
+    for (int i = 0; i < ln / 2; i++)
+    {
+        if (name[i] != name[ln - i - 1])
+        {
+            isTrue = 0;
+            break;
+        }
+    }
+    if (isTrue == 1)
+    {
+        printf("is palindrome");
+    }
+    else
+    {
+        printf("not a palindrome");
+    }
+
+    for (i = 0; name[i] != '\0'; i++)
+    {
+    }
+}
+
+return 0;
---
+++ lect20/diff.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+int main()
+{
+    char name[] = "hiii";
+    printf("name:%c", name[0]);
+
+    int i = 0;
+    while (name[i] != '\0')
+    {
+        printf("\n%c", name[i]);
+        i++
+    }
+
+    return 0;
+}
+
+for (int i = 0; name[i] != '\0'; i++)
+{
+    printf("%c", name[i]);
+}
+
+char data[10];
+for (int i = 0; name[i] != '\0'; i++)
+{
+    data[i] = name[i];
+}
+printf("%s", data);
+
+int ln = strlen(name);
+int isTrue = 1;
+for (int i = 0; i < ln / 2; i++)
+{
+    if (name[i] != name[ln - i - 1])
+    {
+        isTrue = 0;
+        break;
+    }
+}
+if (isTrue == 1)
+{
+    printf("is palindrome");
+}
+else
+{
+    printf("not a palindrome");
+}
+
+for (i = 0; name[i] != '\0'; i++)
+{
+}
+{
+    return
+}
+
