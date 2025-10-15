c3fa95297076e946173f07f61a47d017721e5431
Parikshit Sharma <sharmaparikshit405@gmail.com>
2025-10-15 15:45:59 +0530

    lect25

---
+++ lect25/trapwater.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int trap(int* height, int heightSize) {
+    int left = 0, right = heightSize - 1;
+    int left_max = 0, right_max = 0;
+    int water = 0;
+
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= left_max) {
+                left_max = height[left];
+            } else {
+                water += left_max - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= right_max) {
+                right_max = height[right];
+            } else {
+                water += right_max - height[right];
+            }
+            right--;
+        }
+    }
+
+    return water;
+}
+
+int main() {
+    int height[] = {0,1,0,2,1,0,1,3,2,1,2,1};
+    int size = sizeof(height) / sizeof(height[0]);
+    int result = trap(height, size);
+    printf("Trapped water: %d\n", result);
+    return 0;
+}
